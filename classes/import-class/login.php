85c6dc6189657807fca908aaaacdac3c023173ac
chris <chrismuga94@gmail.com>
2018-09-03 00:37:40 +0300

    >styling...

--- classes/import-class/login.php
+++ classes/import-class/login.php
@@ -10,13 +10,14 @@ require_once('d-base/mysql.php');
 
     <!-- col -->
     <div class = "col-md-4">
+        <div class="alert alert-dark">Student Log-In</div>
         <input type="mail" class="form-control" name="email"  placeholder="E-Mail Address"/>
         <input type="password" class="form-control" name="email"  placeholder="Password"/>
         <hr/>
         <!-- buttons -->
         <div class = "row">
             <div class="col-md-8">
-                <button class="btn btn-success form-control">Enter &nbsp; <i class="fas fa-arrow-right" style="right:0px;"></i> </button>
+                <button class="btn btn-success form-control">Enter &nbsp; <i class="fas fa-arrow-right"></i> </button>
             </div>
             <div class="col-md-4">
                 <button class="btn btn-danger form-control">Clear &nbsp; <i class="fas fa-brush"></i></button>
