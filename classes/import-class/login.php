7eb6e8b0a386ef898a071696f0b25377f078a635
chris <chrismuga94@gmail.com>
2018-09-02 23:20:26 +0300

    >form layout

---
+++ classes/import-class/login.php
@@ -0,0 +1,36 @@
+<?php
+require_once('header.php');
+require_once('d-base/mysql.php');
+
+?>
+
+<!-- row -->
+
+<div class = "row my-2 d-flex justify-content-center">
+
+    <!-- col -->
+    <div class = "col-md-4">
+        <input type="mail" class="form-control" name="email"  placeholder="E-Mail Address"/>
+        <input type="password" class="form-control" name="email"  placeholder="Password"/>
+        <hr/>
+        <!-- buttons -->
+        <div class = "row">
+            <div class="col-md-8">
+                <button class="btn btn-success form-control">Enter</button>
+            </div>
+            <div class="col-md-4">
+                <button class="btn btn-danger form-control">Clear</button>
+            </div>
+        </div>
+        <!-- buttons -->
+    </div>
+    <!-- col -->
+
+</div>
+
+<!-- row -->
+
+<?php
+
+
+?>
