32adaf096a3df0bf581566170ac1604a878f6f73
chris <chrismuga94@gmail.com>
2018-09-02 23:06:30 +0300

    >login

--- classes/import-class/header.php
+++ classes/import-class/header.php
@@ -24,6 +24,7 @@
           <div class="navbar-nav">
             <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link" href="students.php">Students</a>
+            <a class="nav-item nav-link" href="login.php">Login</a>
           </div>
         </div>
       </nav>
