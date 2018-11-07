0cd98431613e4a8c67ce19c8cb5aef862c926f91
Chris Muga <chrismuga94@gmail.com>
2018-11-07 09:49:55 +0300

    dbase backup

--- classes/import-class/dbase-backup/students.sql
+++ classes/import-class/dbase-backup/students.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 19, 2018 at 10:31 AM
--- Server version: 5.7.23-0ubuntu0.18.04.1
--- PHP Version: 7.2.7-0ubuntu0.18.04.2
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2018 at 07:49 AM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,41 +21,45 @@ SET time_zone = "+00:00";
 --
 -- Database: `students`
 --
+CREATE DATABASE IF NOT EXISTS `students` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `students`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `students_info`
+-- Table structure for table `students`
 --
 
-CREATE TABLE `students_info` (
+CREATE TABLE `students` (
   `id` int(250) NOT NULL,
   `name` varchar(250) NOT NULL,
   `phone_number` varchar(250) NOT NULL,
   `location` varchar(250) NOT NULL,
-  `class` int(250) NOT NULL
+  `class` int(250) NOT NULL,
+  `email` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `students_info`
+-- Dumping data for table `students`
 --
 
-INSERT INTO `students_info` (`id`, `name`, `phone_number`, `location`, `class`) VALUES
-(2622, 'Donna Sonia', '849300222', 'Buruburu, Nairobi', 6),
-(4363, 'Tomas Muller', '638798797', 'Munich, Gemany', 1),
-(5762, 'Chris Muga', '0704313126', 'Nairobi, Kenya', 5),
-(6648, 'Nicole Andango', '67474333', 'Buruburu, Naitobi', 8),
-(8755, 'Tom Cruise', '7383393', 'LA, California', 4);
+INSERT INTO `students` (`id`, `name`, `phone_number`, `location`, `class`, `email`) VALUES
+(2622, 'Donna Sonia', '849300222', 'Buruburu, Nairobi', 6, 'donna@mail.com'),
+(4363, 'Tomas Muller', '638798797', 'Munich, Gemany', 1, 'muller@mail.com'),
+(5762, 'Chris Muga', '0704313126', 'Nairobi, Kenya', 5, 'chris@mail.com'),
+(6648, 'Nicole Andango', '67474333', 'Buruburu, Naitobi', 8, 'nicole@mail.com'),
+(8755, 'Tom Cruise', '7383393', 'LA, California', 4, 'tom@mail.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `students_info`
+-- Indexes for table `students`
 --
-ALTER TABLE `students_info`
+ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
